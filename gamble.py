8fb93e29defbd9c9479a10a039b1850fd6a9ab4a
Jordan Jamali <31671594+JJamali@users.noreply.github.com>
2023-11-08 17:49:14 -0500

    Update gamble.py

--- gamble.py
+++ gamble.py
@@ -1,25 +1,42 @@
 import random
 
-money = 45000
-most_money = money
+def simulate():
+  money = 10367
+  most_money = money
+  most_loops = 0
+  loops = 0
+  initial_bet = 50
+  bet = initial_bet
+  
+  while money > 0:
+    loops += 1
+    if random.random() < 0.5:
+      money -= bet
+      bet = min(bet*2, money) 
+    else:
+      money += bet
+      bet = initial_bet
+  
+    if most_money < money:
+      most_money = money
+      most_loops = loops
+
+  # print("max money: " + str(most_money))
+  # print("loops to get to max: " + str(most_loops))
+  # print("amt of flips: " + str(loops))
+
+  return (most_money, most_loops, loops)
+
+most_money = 0
 most_loops = 0
 loops = 0
-initial_bet = 50
-bet = initial_bet
+runs = 30000
+for i in range(runs):
+  sim = simulate()
+  most_money += sim[0]
+  most_loops += sim[1]
+  loops += sim[2]
 
-while money > 0:
-  loops += 1
-  if random.random() < 0.5:
-    money -= bet
-    bet = min(bet*2, money) 
-  else:
-    money += bet
-    bet = initial_bet
+print(most_money / runs, most_loops / runs, loops / runs)
 
-  if most_money < money:
-    most_money = money
-    most_loops = loops
 
-print("max money: " + str(most_money))
-print("loops to get to max: " + str(most_loops))
-print("amt of flips: " + str(loops))
